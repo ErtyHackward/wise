14d1b5ade90b0de398eecb3b91f2e96602cddf0e
Vladislav Pozdnyakov <hackward@gmail.ru>
2020-01-18 22:00:47 +0700

    - fix pulling

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ else
 		build="Debug"
 fi
 
-cd /var/build/wise/ && git reset --hard origin/production && git pull
+cd /var/build/wise/ && git reset --hard origin/production && git pull origin production
 cd /var/build/wise/WiseApi && dotnet publish -c Release
 cd /var/build/wise/WiseBlazor && dotnet publish -c Release
 sudo systemctl restart $service
