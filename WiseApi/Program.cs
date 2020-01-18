c4028dc2bf721c1d981f01d6dba64bcf60948fb5
Vladislav Pozdnyakov <hackward@gmail.ru>
2020-01-18 20:30:03 +0700

    - add Nlog

--- WiseApi/Program.cs
+++ WiseApi/Program.cs
@@ -7,25 +7,16 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Serilog;
-using Serilog.Events;
-using Serilog.Sinks.SystemConsole.Themes;
+using NLog;
 
 namespace WiseApi
 {
     public class Program
     {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
         public static void Main(string[] args)
         {
-            Log.Logger = new LoggerConfiguration()
-                .MinimumLevel.Debug()
-                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
-                .MinimumLevel.Override("System", LogEventLevel.Warning)
-                .MinimumLevel.Override("Microsoft.AspNetCore.Authentication", LogEventLevel.Information)
-                .Enrich.FromLogContext()
-                .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}", theme: AnsiConsoleTheme.Literate)
-                .CreateLogger();
-
+            Logger.Info("Wise service startup");
             var builder = CreateHostBuilder(args);
 
             builder.ConfigureAppConfiguration(b => b.AddEnvironmentVariables("DOTNET_"));
--- deploy.sh
+++ deploy.sh
@@ -10,4 +10,6 @@ else
 fi
 
 cd /var/build/wise/ && git reset --hard origin/production && git pull
+cd /var/build/wise/WiseApi && dotnet publish -c Release
+cd /var/build/wise/WiseBlazor && dotnet publish -c Release
 sudo systemctl restart $service
